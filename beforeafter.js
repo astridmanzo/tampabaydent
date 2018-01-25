d5bf249ba926982c142cee5dd1c7308cbdd911f8
amanzo <astridmanzo@gmail.com>
2018-01-24 21:25:31 -0500

    First commit

---
+++ beforeafter.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+	let wrapper = document.getElementById('.wrapper');
+	let topLayer = wrapper.querySelector('.top');
+	let handle = wrapper.querySelector('.handle');
+	let delta = 0;
+});
+
+wrapper.addEventListener('mousemove', function(e){
+	delta = (e.clientX)
+})
